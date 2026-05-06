e5dc3585502d9b1981b50c2e4de0d1c0270a1c36
owennwb <github@owennwb.com>
2026-05-06 02:02:24 -0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://yidxgspnczekrjiucitx.supabase.co'
-const supabaseAnonKey = sb_publishable_TcmOETQ8ocLZ6pAsJOHwIA_aGtszuto
+const supabaseAnonKey = "sb_publishable_TcmOETQ8ocLZ6pAsJOHwIA_aGtszuto"
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
